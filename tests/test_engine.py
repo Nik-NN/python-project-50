1febecdf488eeb16180cbf4fdac32661192c8550
Nikita Nechaev <no655@yandex.ru>
2023-01-20 20:14:33 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -24,7 +24,10 @@ def tests_change_boolean():
 
 
 def tests_generate_diff():
-    file1 = 'gendiff/file1.json'
-    file2 = 'gendiff/file2.json'
-    result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}'
+    file1 = 'tests/fixtures/file1_for_test.json'
+    file2 = 'tests/fixtures/file2_for_test.json'
+    f = open("tests/fixtures/result_for_generate_diff.txt", "r")
+    result = f.read()
+    #result = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
     assert generate_diff(file1, file2) == result
+    f.close()
