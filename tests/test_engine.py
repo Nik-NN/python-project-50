ff17ff43913fa4af939e73016e41a677adc5d231
Nikita Nechaev <no655@yandex.ru>
2023-03-17 18:30:40 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,22 +1,25 @@
-from gendiff.engine import generate_diff, change_boolean
+from gendiff.engine import generate_diff
 
 
-def tests_generate_diff():
+def tests_generate_diff_stylish():
     file_json1 = 'tests/fixtures/file1_for_test.json'
     file_json2 = 'tests/fixtures/file2_for_test.json'
     file_yml1 = 'tests/fixtures/file1_for_test.yml'
     file_yml2 = 'tests/fixtures/file2_for_test.yml'
-    f = open("tests/fixtures/result_for_generate_diff.txt", "r")
+    f = open("tests/fixtures/result_for_stylish.txt", "r")
     result = f.read()
     assert generate_diff(file_json1, file_json2) == result
     assert generate_diff(file_yml1, file_yml2) == result
     f.close()
 
 
-def tests_change_boolean():
-    value = True
-    value2 = False
-    value3 = None       
-    assert change_boolean(value) == 'true'
-    assert change_boolean(value2) == 'false'
-    assert change_boolean(value3) == 'null'
+def tests_generate_diff_plain():
+    file_json1 = 'tests/fixtures/file1_for_test.json'
+    file_json2 = 'tests/fixtures/file2_for_test.json'
+    file_yml1 = 'tests/fixtures/file1_for_test.yml'
+    file_yml2 = 'tests/fixtures/file2_for_test.yml'
+    f = open("tests/fixtures/result_for_plain.txt", "r")
+    result = f.read()
+    assert generate_diff(file_json1, file_json2, format='plain') == result
+    assert generate_diff(file_yml1, file_yml2, format='plain') == result
+    f.close()
