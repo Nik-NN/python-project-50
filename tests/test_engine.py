89447af1bd3f612d67a86c0a021dd2f076685bb8
Nikita Nechaev <no655@yandex.ru>
2023-03-20 20:38:46 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -8,11 +8,10 @@ def tests_generate_diff():
     file_yml2 = 'tests/fixtures/file2_for_test.yml'
     result_for_stylish = open("tests/fixtures/result_for_stylish.txt", "r").read()
     result_for_plain = open("tests/fixtures/result_for_plain.txt", "r").read()
-    result_for_json = open("tests/fixtures/result_for_json.txt", "r").read()
+    with open("tests/fixtures/result_for_json.json", "r") as my_file:
+        capitals = my_file.read()
     assert generate_diff(file_json1, file_json2) == result_for_stylish
     assert generate_diff(file_yml1, file_yml2) == result_for_stylish
     assert generate_diff(file_json1, file_json2, format='plain') == result_for_plain
     assert generate_diff(file_yml1, file_yml2, format='plain') == result_for_plain
-    assert generate_diff(file_yml1, file_yml2, format='json') == result_for_json
-
-
+    assert generate_diff(file_json1, file_json2, format='json') == capitals
