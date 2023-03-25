55caf4d6d31ccb9446706e59e5ed221e0d8c9ff0
Nikita Nechaev <no655@yandex.ru>
2023-03-25 18:27:09 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,8 +6,6 @@ file_json1 = 'tests/fixtures/file1_for_test.json'
 file_json2 = 'tests/fixtures/file2_for_test.json'
 file_yml1 = 'tests/fixtures/file1_for_test.yml'
 file_yml2 = 'tests/fixtures/file2_for_test.yml'
-file_txt1 = 'tests/fixtures/file1_for_test.txt'
-file_txt2 = 'tests/fixtures/file2_for_test.txt'
 result_for_stylish = open("tests/fixtures/result_for_stylish.txt", "r").read()
 result_for_plain = open("tests/fixtures/result_for_plain.txt", "r").read()
 with open("tests/fixtures/result_for_json.json", "r") as my_file:
@@ -21,7 +19,6 @@ with open("tests/fixtures/result_for_json.json", "r") as my_file:
     (file_json1, file_json2, 'plain', result_for_plain),
     (file_yml1, file_yml2, 'plain', result_for_plain),
     (file_json1, file_json2, 'json', result_for_json),
-    (file_yml1, file_yml2, 'json', result_for_json),
-    (file_txt1, file_txt2, 'stylish', result_for_stylish)])
+    (file_yml1, file_yml2, 'json', result_for_json)])
 def test_generate_diff(test_input_1, test_input_2, format, expected):
     assert generate_diff(test_input_1, test_input_2, format) == expected
