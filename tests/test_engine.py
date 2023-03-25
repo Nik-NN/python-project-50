8ae76ba738784d5b14347c1dc50fe31c9fa24074
Nikita Nechaev <no655@yandex.ru>
2023-03-25 15:41:20 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,17 +1,24 @@
+import pytest
 from gendiff.generate_diff import generate_diff
 
 
-def tests_generate_diff():
-    file_json1 = 'tests/fixtures/file1_for_test.json'
-    file_json2 = 'tests/fixtures/file2_for_test.json'
-    file_yml1 = 'tests/fixtures/file1_for_test.yml'
-    file_yml2 = 'tests/fixtures/file2_for_test.yml'
-    result_for_stylish = open("tests/fixtures/result_for_stylish.txt", "r").read()
-    result_for_plain = open("tests/fixtures/result_for_plain.txt", "r").read()
-    with open("tests/fixtures/result_for_json.json", "r") as my_file:
-        capitals = my_file.read()
-    assert generate_diff(file_json1, file_json2) == result_for_stylish
-    assert generate_diff(file_yml1, file_yml2) == result_for_stylish
-    assert generate_diff(file_json1, file_json2, format='plain') == result_for_plain
-    assert generate_diff(file_yml1, file_yml2, format='plain') == result_for_plain
-    assert generate_diff(file_json1, file_json2, format='json') == capitals
+file_json1 = 'tests/fixtures/file1_for_test.json'
+file_json2 = 'tests/fixtures/file2_for_test.json'
+file_yml1 = 'tests/fixtures/file1_for_test.yml'
+file_yml2 = 'tests/fixtures/file2_for_test.yml'
+result_for_stylish = open("tests/fixtures/result_for_stylish.txt", "r").read()
+result_for_plain = open("tests/fixtures/result_for_plain.txt", "r").read()
+with open("tests/fixtures/result_for_json.json", "r") as my_file:
+    result_for_json = my_file.read()
+
+
+@pytest.mark.parametrize("test_input_1,test_input_2,format,expected", [
+    (file_json1, file_json2, 'stylish', result_for_stylish),
+    (file_yml1, file_yml2, 'stylish', result_for_stylish),
+    (file_json1, file_yml2, 'stylish', result_for_stylish),
+    (file_json1, file_json2, 'plain', result_for_plain),
+    (file_yml1, file_yml2, 'plain', result_for_plain),
+    (file_json1, file_json2, 'json', result_for_json),
+    (file_yml1, file_yml2, 'json', result_for_json)])
+def test_generate_diff(test_input_1, test_input_2, format, expected):
+    assert generate_diff(test_input_1, test_input_2, format) == expected
