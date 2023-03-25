a5f5c87c11b3f7ad99720aaa1e02b1fd7ff72e81
Nikita Nechaev <no655@yandex.ru>
2023-03-25 18:12:34 +0300

    update gendiff

--- gendiff/formatters/plain_formatter.py
+++ gendiff/formatters/plain_formatter.py
@@ -12,28 +12,26 @@ def change_boolean(value):
     return value
 
 
-def get_plain_diff(diff):
-    result = plain(diff)
-    return result[:-1]
-
-
-def plain(diff):
-    result = ''
-    for item in diff:
-        if item['status'] == 'parent':
-            value = plain(item['children'])
-            result += f"{value}"
-        elif item['status'] == 'added':
-            path = (item['path'])[:-1]
-            value = item['value']
-            value = change_boolean(value)
-            result += f"Property '{path}' was added with value: {value}\n"
-        elif item['status'] == 'deleted':
-            path = (item['path'])[:-1]
-            result += f"Property '{path}' was removed\n"
-        elif item['status'] == 'changed':
-            path = (item['path'])[:-1]
-            val1 = change_boolean(item['value1'])
-            val2 = change_boolean(item['value2'])
-            result += f"Property '{path}' was updated. From {val1} to {val2}\n"
-    return result
+def get_plain_diff(diff):  # noqa: C901
+    def inner(diff):
+        result = ''
+        for item in diff:
+            if item['status'] == 'parent':
+                value = inner(item['children'])
+                result += f"{value}"
+            elif item['status'] == 'added':
+                path = (item['path'])[:-1]
+                value = item['value']
+                value = change_boolean(value)
+                result += f"Property '{path}' was added with value: {value}\n"
+            elif item['status'] == 'deleted':
+                path = (item['path'])[:-1]
+                result += f"Property '{path}' was removed\n"
+            elif item['status'] == 'changed':
+                path = (item['path'])[:-1]
+                val1 = change_boolean(item['value1'])
+                val2 = change_boolean(item['value2'])
+                result += f"Property '{path}' was updated. "
+                f"From {val1} to {val2}\n"
+        return result
+    return inner(diff).rstrip()
