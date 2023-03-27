88c94f68b4924d5c3569ebfac295c4ccba129fe3
Nikita Nechaev <no655@yandex.ru>
2023-03-27 18:59:28 +0300

    update gendiff

--- gendiff/argparse.py
+++ gendiff/argparse.py
@@ -1,12 +1,13 @@
 import argparse
 
 
-def arg_parse():
+def arg_parse(argv=None):
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('-f', '--format', default='stylish',
                         help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
+    print(args)
     return args
